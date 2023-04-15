217570d218cc0bc7e3ab325fea7d6a9d36347215
Nishant Kumar <nishants878@gmail.com>
2023-04-15 19:05:05 +0530

    Input done

--- frontend/components/core-components/input/Input.tsx
+++ frontend/components/core-components/input/Input.tsx
@@ -1,33 +1,146 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
+import {
+  InputLabel,
+  TextField,
+  FormHelperText,
+  FormControl,
+} from "@mui/material";
 import Box from "@mui/material/Box";
 import { Main } from "@/components/features/test-page/TestComponent.style";
+import styled from "@emotion/styled";
+
 interface IInputTypes {
-  label: string;
+  label?: string;
+  placeholder?: string;
+  type?: string;
+  inputProps?: any;
+  formControlProps?: any;
+  error?: boolean;
+  onChange: () => void;
+  value: string;
+  helperText?: string;
+  required?: boolean;
+  disabled?: boolean;
 }
 
-const Input = ({ label }: IInputTypes) => {
+const CustomInput = styled(TextField)`
+  .MuiFormHelperText-root {
+    color: #a5a5a5;
+    font-size: 12px;
+    font-family: "poppins";
+  }
+  .MuiInputBase-root {
+    border-radius: 22px;
+  }
+
+  & label.Mui-focused {
+    color: white;
+  }
+  & .MuiOutlinedInput-root {
+    &.Mui-focused fieldset {
+      border-color: #ffffff;
+      border-width: 1px;
+    }
+  }
+  & .MuiOutlinedInput-root:hover {
+    & > fieldset {
+      border-color: #ffffff;
+      border-width: 1px;
+    }
+  }
+
+  & .MuiOutlinedInput-root {
+    &.Mui-disabled fieldset {
+      background: radial-gradient(
+        100% 100% at 0% 0%,
+        rgba(255, 255, 255, 0.04) 0%,
+        rgba(255, 255, 255, 0) 100%
+      );
+
+      /* backdrop-filter: blur(21px); */
+    }
+  }
+  & .MuiOutlinedInput-root:hover {
+    &.Mui-disabled fieldset {
+      border: 1px solid #a5a5a5;
+    }
+  }
+`;
+
+const Input = ({
+  label,
+  type,
+  inputProps,
+  formControlProps,
+  error,
+  onChange,
+  value,
+  helperText,
+  required,
+  placeholder,
+  disabled,
+}: IInputTypes) => {
+  console.log("error", error);
   return (
-    <TextField
-      placeholder={label}
-      sx={{
-        outline: "0",
-        backdropFilter: "blur(21px)",
-        input: {
+    <FormControl>
+      {label !== undefined ? (
+        <InputLabel
+          sx={{
+            fontSize: "14px",
+            fontWeight: "500",
+            color: "#ffffff",
+            fontFamily: "poppins",
+            marginBottom: "var(--spacing-1)",
+          }}
+          shrink={true}
+          htmlFor={inputProps?.name}
+        >
+          {label}
+          {required && <span className="label_astrik">*</span>}
+        </InputLabel>
+      ) : null}
+      <CustomInput
+        placeholder={placeholder}
+        type={type}
+        id={inputProps?.name}
+        error={error}
+        onChange={onChange}
+        value={value}
+        helperText={helperText}
+        required={required}
+        fullWidth
+        disabled={disabled}
+        // focused={true}
+        sx={{
           outline: "0",
-          padding: "var(--spacing-3)",
-          borderRadius: "22px",
-          border: "1px solid grey",
-          boxShadow: "inset -5px -5px 250px rgba(255, 255, 255, 0.02)",
-          background:
-            "radial-gradient(100% 100% at 0% 0%, rgba(255, 255, 255, 0.16) 0%, rgba(255, 255, 255, 0) 100%)",
-          "&::placeholder": {
-            textOverflow: "ellipsis !important",
+          width: "350px",
+          fontFamily: "poppins",
+          backdropFilter: "blur(21px)",
+          marginTop: "10px",
+          border: "0",
+          input: {
+            outline: "0",
+            padding: "var(--spacing-3)",
             color: "#A5A5A5",
+            borderRadius: "22px",
+            fontFamily: "poppins",
+            border: "1px solid #a5a5a5",
+            boxShadow: "inset -5px -5px 250px rgba(255, 255, 255, 0.02)",
+            background:
+              "radial-gradient(100% 100% at 0% 0%, rgba(255, 255, 255, 0.16) 0%, rgba(255, 255, 255, 0) 100%)",
+            backdropFilter: "blur(21px)",
+            "&::placeholder": {
+              textOverflow: "ellipsis !important",
+              color: "#A5A5A5",
+            },
+            "&:focused": {
+              background:
+                "radial-gradient(100% 100% at 0% 0%, rgba(255, 255, 255, 0.16) 0%, rgba(255, 255, 255, 0) 100%)",
+            },
           },
-        },
-      }}
-    />
+        }}
+      />
+    </FormControl>
   );
 };
 
--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,7 +1,41 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import { Formik, Form, FastField } from "formik";
+import FastInput from "@/components/core-components/input/FastInput";
+import * as Yup from "yup";
+import Input from "@/components/core-components/input/Input";
 const register = () => {
-  return <div>register</div>;
+  const formikRef = useRef();
+
+  const handleSubmit = (values: any, actions: any) => {
+    console.log("onsubmit", values);
+  };
+  return (
+    <div style={{ background: "black" }}>
+      <p>Formik test</p>
+      {/* <FastInput name={"dr"} /> */}
+      <Formik
+        validateOnMount
+        initialValues={{ email: "", name: "" }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string().email().required(),
+        })}
+        onSubmit={handleSubmit}
+      >
+        {(props) => {
+          const { email, name } = props.values;
+          return (
+            <form onSubmit={props.handleSubmit}>
+              <FastInput name="email" label="Email" required />
+              <FastInput name="name" />
+              <button onClick={props.submitForm} type="button">
+                submit
+              </button>
+            </form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
 };
 
 export default register;
