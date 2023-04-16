9c2c87d1af8cf5c367195034ba3d77f69ea79eaa
SANKET <32214164+sanket8088@users.noreply.github.com>
2023-04-16 19:56:36 +0530

    added cors headers

--- demlo-backend/backend/backend/settings.py
+++ demlo-backend/backend/backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'rest_framework.authtoken',
     'rest_framework',
     'user',
